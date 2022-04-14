c5402619248e78b1b863b3b4b9798a0c2f8e79db
leoric-crown <leon.ricardo314@gmail.com>
2022-04-14 01:20:02 -0500

    Push to Vercel

--- next/lib/firebase.ts
+++ next/lib/firebase.ts
@@ -3,7 +3,6 @@ import { getFirestore } from "firebase/firestore";
 import firebaseConfig from "../firebaseConfig";
 
 const firebaseApp = initializeApp(firebaseConfig);
-
 export const firestore = getFirestore(firebaseApp);
 
 export default firebaseApp;
